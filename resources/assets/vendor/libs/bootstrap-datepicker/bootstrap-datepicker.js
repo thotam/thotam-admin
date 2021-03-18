fe8f8a96abee46f0b0359ae4664a19b4ce6322c3
thanhtamtotaa <thanhtamtqno1@gmail.com>
2021-03-18 14:25:10 +0700

    Add locales Vi to bootstrap-datepicker

--- resources/assets/vendor/libs/bootstrap-datepicker/bootstrap-datepicker.js
+++ resources/assets/vendor/libs/bootstrap-datepicker/bootstrap-datepicker.js
@@ -1,1 +1,2 @@
-import 'bootstrap-datepicker/dist/js/bootstrap-datepicker.js'
+import "bootstrap-datepicker/dist/js/bootstrap-datepicker.js";
+import "bootstrap-datepicker/dist/locales/bootstrap-datepicker.vi.min.js";
