3ba0c6d103ced9dddb22512da4690defae8738e8
thanhtamtotaa <thanhtamtqno1@gmail.com>
2021-05-03 17:30:08 +0700

    add livewire datatables lang and view

---
+++ resources/lang/vendor/LivewireDatatableBs4/vi/datatable.php
@@ -0,0 +1,24 @@
+<?php
+return [
+    'export' => 'Export',
+    'loading' => 'Đang tải...',
+    'search' => 'Tìm kiếm trong cột: :columns',
+    'select_all' => 'Chọn tất cả',
+    'no_data_table' => 'Không có dữ liệu để hiển thị',
+    'pagination_text' => 'Hiện thị :start - :end trên :total',
+    'prev' => 'Trước',
+    'next' => 'Sau',
+    'all' => 'Tất cả',
+    'hide' => 'Ẩn',
+    'show' => 'Hiện',
+    'table_col' => 'Các cột trong bảng',
+    'no' => 'Không',
+    'yes' => 'Có',
+    'min' => 'Tối thiểu',
+    'max' => 'Tối đa',
+    'choose' => 'Chọn...',
+    'type_find' => 'Nhập vào đây để tìm kiếm',
+    'modal_delete_title' => 'Xóa :id',
+    'modal_delete_info' => 'Bạn có chắc chắn?',
+    'deleting' => 'Đang xóa...'
+];
