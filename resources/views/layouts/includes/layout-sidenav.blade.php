b0f5cfa1747ace17fe0eb5d6bb603bf5d9a08657
thanhtamtotaa <thanhtamtqno1@gmail.com>
2021-05-24 16:38:37 +0700

    Update layout-sidenav.blade.php

--- resources/views/layouts/includes/layout-sidenav.blade.php
+++ resources/views/layouts/includes/layout-sidenav.blade.php
@@ -1,3 +1,5 @@
+<?php $routeName = Route::currentRouteName(); ?>
+
 <div id="layout-sidenav" class="{{ isset($layout_sidenav_horizontal) ? 'layout-sidenav-horizontal sidenav-horizontal container-p-x flex-grow-0' : 'layout-sidenav sidenav-vertical' }} sidenav bg-dark">
 
     @empty($layout_sidenav_horizontal)
@@ -19,11 +21,108 @@
     <ul class="sidenav-inner{{ empty($layout_sidenav_horizontal) ? ' py-1' : '' }}">
 
         <li class="sidenav-item{{ Request::is('/') ? ' active' : '' }}">
-            <a href="{{ route('home') }}" class="sidenav-link"><i class="sidenav-icon ion ion-ios-contact"></i><div>Home</div></a>
+            <a href="{{ route('home') }}" class="sidenav-link"><i class="sidenav-icon fas fa-home d-block"></i><div>Trang chủ</div></a>
         </li>
 
-        <li class="sidenav-item{{ Request::is('page-2') ? ' active' : '' }}">
-            <a href="{{ route('page-2') }}" class="sidenav-link"><i class="sidenav-icon ion ion-md-desktop"></i><div>Page 2</div></a>
+        <!-- Admin -->
+        @if (Auth::user()->hr->hasAnyRole(["super-admin", "admin"]))
+            <li class="sidenav-item{{ strpos($routeName, 'admin.') === 0 ? ' active open' : '' }}">
+                <a href="javascript:void(0)" class="sidenav-link sidenav-toggle"><i class="sidenav-icon fas fa-users-cog d-block"></i><div>Admin</div></a>
+
+                <ul class="sidenav-menu">
+
+                        <li class="sidenav-item{{ strpos($routeName, 'admin.member.') === 0 ? ' active open' : '' }}">
+                            <a href="javascript:void(0)" class="sidenav-link sidenav-toggle"><i class="sidenav-icon fas fa-user-friends d-block"></i><div>Thành viên</div></a>
+
+                            <ul class="sidenav-menu">
+
+                                <li class="sidenav-item{{ $routeName == 'admin.member.user' ? ' active' : '' }}">
+                                    <a href="{{ route('admin.member.user') }}" class="sidenav-link"><i class="sidenav-icon far fa-user-circle d-block"></i><div>Tài khoản</div></a>
+                                </li>
+
+                                <li class="sidenav-item{{ $routeName == 'admin.member.hr' ? ' active' : '' }}">
+                                    <a href="{{ route('admin.member.hr') }}" class="sidenav-link"><i class="sidenav-icon fas fa-user-lock d-block"></i><div>Nhân sự</div></a>
+                                </li>
+
+                                <li class="sidenav-item{{ $routeName == 'admin.member.team' ? ' active' : '' }}">
+                                    <a href="{{ route('admin.member.team') }}" class="sidenav-link"><i class="sidenav-icon fas fa-users d-block"></i><div>Nhóm</div></a>
+                                </li>
+
+                            </ul>
+
+                        </li>
+
+                        <li class="sidenav-item{{ strpos($routeName, 'admin.permission.') === 0 ? ' active open' : '' }}">
+                            <a href="javascript:void(0)" class="sidenav-link sidenav-toggle"><i class="sidenav-icon fas fa-users-cog d-block"></i><div>Phân quyền</div></a>
+
+                            <ul class="sidenav-menu">
+
+                                <li class="sidenav-item{{ $routeName == 'admin.permission.permission' ? ' active' : '' }}">
+                                    <a href="{{ route('admin.permission.permission') }}" class="sidenav-link"><i class="sidenav-icon fas fa-user-cog d-block"></i><div>Permission</div></a>
+                                </li>
+
+                                <li class="sidenav-item{{ $routeName == 'admin.permission.role' ? ' active' : '' }}">
+                                    <a href="{{ route('admin.permission.role') }}" class="sidenav-link"><i class="sidenav-icon fas fas fa-user-tag d-block"></i><div>Role</div></a>
+                                </li>
+                            </ul>
+                        </li>
+
+                </ul>
+            </li>
+        @endif
+
+        <!-- Buddy -->
+        <li class="sidenav-item{{ strpos($routeName, 'buddy.') === 0 ? ' active open' : '' }}">
+            <a href="javascript:void(0)" class="sidenav-link sidenav-toggle"><i class="sidenav-icon fas fa-user-friends d-block"></i><div>Buddy</div></a>
+
+            <ul class="sidenav-menu">
+
+                <li class="sidenav-item{{ $routeName == 'buddy.canhan' ? ' active' : '' }}">
+                    <a href="{{ route('buddy.canhan') }}" class="sidenav-link"><i class="sidenav-icon fas fa-user d-block"></i><div>Cá nhân</div></a>
+                </li>
+
+                <li class="sidenav-item{{ $routeName == 'buddy.nhom' ? ' active' : '' }}">
+                    <a href="{{ route('buddy.nhom') }}" class="sidenav-link"><i class="sidenav-icon fas fas fa-users d-block"></i><div>Nhóm</div></a>
+                </li>
+            </ul>
+        </li>
+
+        <!-- TT-MKT -->
+        <li class="sidenav-item{{ strpos($routeName, 'mkt.') === 0 ? ' active open' : '' }}">
+            <a href="javascript:void(0)" class="sidenav-link sidenav-toggle"><i class="sidenav-icon fas fa-bullseye d-block"></i><div>TT-MKT</div></a>
+
+            <ul class="sidenav-menu">
+
+                <li class="sidenav-item{{ $routeName == 'mkt.nhom' ? ' active' : '' }}">
+                    <a href="{{ route('mkt.nhom') }}" class="sidenav-link"><i class="sidenav-icon fas fa-users d-block"></i><div>Nhóm</div></a>
+                </li>
+
+                <li class="sidenav-item{{ strpos($routeName, 'mkt.congviec.') === 0 ? ' active open' : '' }}">
+                    <a href="javascript:void(0)" class="sidenav-link sidenav-toggle"><i class="sidenav-icon fas fa-cogs d-block"></i><div>Công việc</div></a>
+
+                    <ul class="sidenav-menu">
+
+                        <li class="sidenav-item{{ $routeName == 'mkt.congviec.yeucau' ? ' active' : '' }}">
+                            <a href="{{ route('mkt.congviec.yeucau') }}" class="sidenav-link"><i class="sidenav-icon fab fa-elementor d-block"></i><div>Yêu cầu</div></a>
+                        </li>
+
+                        <li class="sidenav-item{{ $routeName == 'mkt.congviec.phancong' ? ' active' : '' }}">
+                            <a href="{{ route('mkt.congviec.phancong') }}" class="sidenav-link"><i class="sidenav-icon fas fa-tasks d-block"></i><div>Phân công</div></a>
+                        </li>
+
+                        <li class="sidenav-item{{ $routeName == 'mkt.congviec.baocao' ? ' active' : '' }}">
+                            <a href="{{ route('mkt.congviec.baocao') }}" class="sidenav-link"><i class="sidenav-icon fas fa-file-upload d-block"></i><div>Báo cáo</div></a>
+                        </li>
+
+                        <li class="sidenav-item{{ $routeName == 'mkt.congviec.danhgia' ? ' active' : '' }}">
+                            <a href="{{ route('mkt.congviec.danhgia') }}" class="sidenav-link"><i class="sidenav-icon far fa-check-square d-block"></i><div>Đánh giá</div></a>
+                        </li>
+
+                    </ul>
+
+                </li>
+
+            </ul>
         </li>
 
     </ul>
