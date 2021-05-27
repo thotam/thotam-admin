ae38dff3059710364d467319daa97fb495baab43
thanhtamtotaa <thanhtamtqno1@gmail.com>
2021-05-27 17:11:41 +0700

    Create chuky.blade.php

---
+++ resources/views/mail/chuky.blade.php
@@ -0,0 +1,10 @@
+<div dir='ltr' data-smartmail='gmail_signature'>
+    <span>--</span>
+    <br>
+    <font color='#888888'>Bạn nhận được thư này từ phòng 'Truyền Thông - Marketing - CPC1HN'.
+    <br>Thư được gửi tự động bởi:
+    <a target="_blank" href='{{ route('home') }}'>Website Quản lý Truyền thông - CPC1 Hà Nội</a>.
+    <br>
+    </font>
+    <span>--</span>
+</div>
