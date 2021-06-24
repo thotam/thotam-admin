97dbd9c061a3dc7bd81abbc156fadaf67215eb38
thanhtamtotaa <thanhtamtqno1@gmail.com>
2021-06-24 13:30:21 +0700

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -40,6 +40,7 @@ class Kernel extends ConsoleKernel
         $schedule->command('thotam-icpc1hn-api:icpc1hn-renew-token')->dailyAt('05:30')->withoutOverlapping(15);
 
         $schedule->command('thotam-mkt-kpi:generate-tdtp')->twiceMonthly(1, 25,'07:23');
+        $schedule->command('thotam-mkt-kpi:generate-kpi-canhan')->dailyAt('00:13');
 
         $schedule->command('queue:restart')->hourly();
         $schedule->command('queue:retry all')->everySixHours();
